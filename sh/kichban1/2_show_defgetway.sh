52c1397d34967383f6a622a4afd010e5a6fd1f64
vanduan <vanduan95.dvp@gmail.com>
2017-02-17 00:20:17 +0700

    Add files via upload

---
+++ sh/kichban1/2_show_defgetway.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+[ $# = 1 ] || {
+	echo "Usage: $0 <ip address>";
+	exit 1;
+}
+
+# check ip
+isvalidip(){
+	case $1 in "" | *[!0-9.]* | *[!0-9]) return 1;;
+	esac
+	local IFS=.
+	set -- $1
+	
+	[ $# -eq 4 ] && [ ${1:-666} -le 255 ] && [ ${2:-666} -le 255 ] && [ ${3:-666} -le 255 ] && [ ${4:-666} -le 255 ]
+}
+
+if ! isvalidip "$1"
+then
+	echo "Invalid IP address"
+	exit 1
+fi
+
+# check connection
+ping $1 -c3 > /dev/null
+[ $? = 0 ] || {
+	echo "Can't connect to $1";
+	exit 1;
+}
+
+# main - get default getway
+gw=$(ssh $1 route | grep default | awk '{printf $2 }')
+echo "Default getway: $gw"
