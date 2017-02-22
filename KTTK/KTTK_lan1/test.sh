728bccd8560f8d45febb1f1b715fb0e516a89fd6
vanduan <vanduan95.dvp@gmail.com>
2017-02-22 22:54:34 +0700

    Update test.sh
    đã sửa theo cách của thầy, nhưng chưa test thử

--- KTTK/KTTK_lan1/test.sh
+++ KTTK/KTTK_lan1/test.sh
@@ -5,16 +5,11 @@
 	exit 1;
 }
 
-while read line
+while IFS=- read pc
+login_user new_name service
 do
-	# lấy thông tin từ file
-	pc=$(echo $line | cut -d'-' -f1)
-	login_user=$(echo $line | cut -d'-' -f2)
-	new_name=$(echo $line | cut -d'-' -f3)
-	service=$(echo $line | cut -d'-' -f4)
 	# lấy ip và hostname từ /etc/host
 	ip=$(grep $pc /etc/hosts | head -n1 | cut -f1)
-	old_name=$(grep $pc /etc/hosts | head -n1 | cut -f2)
 	# tạo file KQ.txt
 	echo "<tên cũ> - <địa chỉ IP> - <tên mới> - <Yes/No tương ứng với dịch vụ chạy/không>" > KQ.txt;
 	# kiểm tra kết nối tới pc
@@ -24,11 +19,18 @@ do
 		echo "$old_name - $ip - Can't connect - No";
 		continue;
 	}
+	# lấy tên hostname cũ
+	old_name=$(ssh $login_user@pc hostname)
 	# đổi tên hostname
 	ssh $login_user@$pc hostname "$new_name"
-	# không biết làm :))
-	service_status="Unknow"
-	
+	# trạng thái service
+	result=$(ssh $login_user@$pc service $service status | grep "running" )
+	if $result ! = ""
+	then
+		service_status="Yes"
+	else
+		service_status="No"
+	fi
 	echo "$old_name - $ip - $new_name - $service_status"
 	echo "$old_name - $ip - $new_name - $service_status" >> KQ.txt
 done < $1
